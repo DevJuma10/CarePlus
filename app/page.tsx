6059b0e22e58231a012eff92224d5d2ffbd9f746
DevJuma10 <juma.study@gmail.com>
2024-07-30 17:38:35 +0300

    Project Scafold

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+
+
+export default function Home() {
+  return (
+    <div>
+      <div className="text-white">HOME</div>
+      <Button variant='outline'>Button</Button>
+    </div>
+    
+  );
+}
