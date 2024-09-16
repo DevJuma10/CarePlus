2405d7e25fa56e21bc2936506123104f4f144810
DevJuma10 <juma.study@gmail.com>
2024-09-16 21:07:54 +0300

    Feat: Add admin dashboard

---
+++ app/admin/page.tsx
@@ -0,0 +1,67 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import logo from '../../public/assets/icons/logo-full.svg'
+import React from 'react'
+import StatCard from '@/components/ui/statCard'
+import appointmentsIcon from '../../public/assets/icons/appointments.svg'
+import pendingIcon from '../../public/assets/icons/pending.svg'
+import cancelledIcon from '../../public/assets/icons/cancelled.svg'
+import { getRecentAppointmentList } from '@/lib/actions/appointment.actions'
+
+
+const Admin = async () => {
+
+    const appointments = await getRecentAppointmentList()
+
+  return (
+    <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
+        <header className="admin-header">
+
+            <Link href='/'>
+
+                <Image 
+                    src={logo}
+                    height={32}
+                    width={162}
+                    alt='logo'
+                    className='h-8 w-fit'
+                />
+            </Link>
+            
+            <p className="text-16-semibold">Admin Dashboard</p>
+        </header>
+
+        <main className="admin-main">
+            <section className="w-full space-y-4">
+                <h1 className="header">Welcome</h1>
+                <p className="text-dark-700">Start the day with managing new appointments</p>
+            </section>
+
+            <section className="admin-stat">
+                <StatCard 
+                    type='appointments'
+                    count={appointments.scheduledCount}
+                    label='Scheduled Appointments'
+                    icon={appointmentsIcon}
+                />
+
+                <StatCard 
+                    type='pending'
+                    count={appointments.pendingCount}
+                    label='pending Appointments'
+                    icon={pendingIcon}
+                />
+
+                <StatCard 
+                    type='cancelled'
+                    count={appointments.cancelledCount}
+                    label='cancelled Appointments'
+                    icon={cancelledIcon}
+                />
+            </section>
+        </main>
+    </div>
+  )
+}
+
+export default Admin
